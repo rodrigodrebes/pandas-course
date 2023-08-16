eaa78e55563607181ce5b156beeae1b2561a988f
Rodrigo <rodrigodrebes97@gmail.com>
2023-08-16 13:07:30 -0300

    column and row methods

--- basics/create_csv.py
+++ basics/create_csv.py
@@ -11,3 +11,20 @@ print(df.tail())
 
 # .columns = all the columns of the file
 print(df.columns)
+
+# . dtypes = the types of columns
+print(df.dtypes)
+
+# .describe() summary statistics of data
+print(df.describe())
+
+# [''] or [['','']] = access specific column
+print(df['Age'])
+print(df[['Name', 'Age']])
+
+# .unique = unique values
+print(df['Age'].unique())
+
+# filtering specific row value
+print(df[df['Age'] == 33])
+print(df[(df['Name'] == "Rodrigo") & (df['Age'] == 34)])
